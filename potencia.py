41c8ae68768e3e25d1340dbcc1ebf789a52cdf8d
Aluno <aluno@ifpbsr.com>
2019-02-21 09:59:55 -0300

    aaaaa

---
+++ potencia.py
@@ -0,0 +1,21 @@
+def main():
+    ''' Programa para teste da sua funcao potencia '''
+    base = float(input("Digite a base real: "))
+    exp  = int(input("Digite o expoente inteiro: "))
+    pot  = potencia(base, exp)
+    print("potencia(%f, %d) = %f"%(base, exp, pot))
+
+def potencia(base, expoente):
+    ''' (float, int) -> float
+    retorna a base elevado ao expoente inteiro '''
+
+    resultado = base
+    n = 1
+    while n < expoente:
+        resultado = resultado * base
+        n = n + 1
+    return resultado
+
+main()
+
+
